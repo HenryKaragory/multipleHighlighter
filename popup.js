0932b4037ef55ad712eb386ad097bb97e51d1db1
Henry Karagory <henrytkaragory@gmail.com>
2017-09-05 20:20:45 -0400

    Added local storage functionality so that custom colors will be saved. Added functionality for managing custom colors so that they may be removed by the user. Added functionality to ensure that when custom colors are removed the option nodes for the custom color are removed from each current select node. Updated functionality for allowing the user to search for multiple words with both custom and base colors.

--- popup.js
+++ popup.js
@@ -1,23 +1,190 @@
 var numberOfWords = 1;
 
+/*
+*------------------------------------------------
+* Functions for toggling and changing the display
+*------------------------------------------------
+*/
+
+// Toggle the display of the new color section
+function toggleRGBInputDisplay(){
+	var rgbInputDiv = document.getElementById('rgb-input');
+	var currentDisplay = rgbInputDiv.style.display;
+	
+	if(currentDisplay == "none"){
+		rgbInputDiv.style.display = "block";
+	}else{
+		rgbInputDiv.style.display = "none";
+	}
+}
+
+// Toggle the display of the manage colors section and add all current custom colors to this section.
+function toggleManageColorDisplay(){
+	var manageColorSection = document.getElementById('manage-colors-display');
+	var display = manageColorSection.style.display;
+
+	// Toggle the display to none if it is block. Otherwise set the display to
+	// block and put all current custom colors into buttons in the manage color section.
+	if(display=="block"){
+		manageColorSection.style.display="none"
+		while(manageColorSection.hasChildNodes()){
+			manageColorSection.removeChild(manageColorSection.lastChild);
+		}
+	}else{
+		manageColorSection.style.display = "block";
+		// Get all custom colors from storage.
+		chrome.storage.sync.get(null,function(items){
+					
+			var isEmpty = true;
+			for(var key in items){
+				isEmpty = false;				
+				var removeButton = document.createElement("button");
+
+				removeButton.innerHTML = 'Remove: '+key;
+				removeButton.value=key;
+				removeButton.style.color = items[key];
+				removeButton.type="button";
+				removeButton.style.marginTop="5px";
+				removeButton.style.marginLeft="10px";
+				removeButton.className="remove-word";
+
+				manageColorSection.appendChild(removeButton);
+				manageColorSection.appendChild(document.createElement("br"));
+			}
+			if(isEmpty){
+				manageColorSection.innerHTML = "<div style=\"margin-left:10px;margin-top:5px\"> <b>You have no custom colors!</b></div>";
+			}
+		});
+	}
+}
+
+// Removes a given custom color from storage.
+function removeColorFromStorage(color){
+	chrome.storage.sync.remove(color);
+}
+
+// Removes a given custom color option node from each select node
+function removeColorFromAllSelectNodes(color){
+	var optionNodes = document.querySelectorAll('option#'+color)
+	console.log(optionNodes[0])
+	console.log(optionNodes[0].parentNode)
+	console.log(color)
+	for(var i=0;i<optionNodes.length;i++){
+		optionNodes[i].parentNode.removeChild(optionNodes[i])
+	}
+}
+
+/*
+* This function gets the name of the new color and the rgb values and then updates all the 
+* drop down menus with the name of the new color.
+*/
+function createNewColor(){
+	var newColorName = document.getElementById('new-color-name').value;
+	var redValue = document.getElementById('new-red-value').value;
+	var greenValue = document.getElementById('new-green-value').value;
+	var blueValue = document.getElementById('new-blue-value').value;
+	
+	// Reset all of the values and the color of the color preview
+	document.getElementById('new-color-name').value = "";
+	document.getElementById('new-red-value').value = "";
+	document.getElementById('new-blue-value').value = "";
+	document.getElementById('new-green-value').value = "";
+	document.getElementById('color-preview').style.color = "black";
+	
+	var rgbString = "rgba("+redValue+","+greenValue+","+blueValue+",0.5)";
+
+	var allSelectTags = document.getElementsByTagName("select");
+	
+	// For each select tag append the name of the new color as an option
+	for(var i=0;i<allSelectTags.length;i++){
+		var newOption = document.createElement("option");
+		var optionText = document.createTextNode(newColorName);
+		newOption.setAttribute("value",rgbString);
+		newOption.setAttribute("id",newColorName);
+		newOption.appendChild(optionText);
+		
+		allSelectTags[i].appendChild(newOption);
+	}	
+	toggleRGBInputDisplay();
+
+	var colorNameValueObject = {};
+	colorNameValueObject[newColorName] = rgbString;	
+	chrome.storage.sync.set(colorNameValueObject);
+}
+
+// This updates the color preview when values have been enetered for blue, green, and red
+function updateColorPreview(){
+	var blueValue = document.getElementById("new-blue-value").value;
+	var redValue = document.getElementById("new-red-value").value;
+	var greenValue = document.getElementById("new-green-value").value;
+
+	if(blueValue && redValue && greenValue){
+		var colorPreview = document.getElementById("color-preview");
+		colorPreview.style.color = "rgb("+redValue+","+greenValue+","+blueValue+")";
+	}	
+}
+
 // Adds another word to the extension
 function newWord(){
+	numberOfWords+=1;		
+
 	var wordTemplate = document.getElementById("word-template").innerHTML;
 	var newWordButton = document.getElementById("another-word");
 	var newDiv = document.createElement("div");
+	var removeButton = document.createElement("button");
 	var wordTemplateParent = document.getElementById("the-form");
+	
+	// TODO: Fix this class name so that it doesn't conflict with the remove-word for removing
+	// from memory
+	removeButton.className ="remove-word";
+	removeButton.innerHTML = "Remove";
+	removeButton.setAttribute('type','button');
+	removeButton.addEventListener('click',function(e){
+		var remButton = e.target;
+		remButton.parentNode.parentNode.removeChild(remButton.parentNode);		
+	});
 
-	numberOfWords+=1;		
 	newDiv.innerHTML = wordTemplate;
 	newDiv.children[0].id = "word-"+numberOfWords;
-	
-	
+	newDiv.style.marginTop = "10px";	
+	newDiv.style.marginBottom = "10px";
+	newDiv.appendChild(removeButton);
+
 	wordTemplateParent.insertBefore(newDiv,newWordButton);
-	wordTemplateParent.insertBefore(document.createElement("br"),newWordButton);
 }
 
+// This function is called by the event listener for the remove word button.
+// It removes the word from the DOM.
+function removeNode(nodeToRemove){
+	nodeToRemove.style.display="none";
+	nodeToRemove.parentNode.removeChild(nodeToRemove);
+}
+
+// Adds all current custom colors in chrome storage to the select element in word-template
+function addCustomColorsToSelect(){
+	var wordTemplate = document.getElementById('word-template');
+	var selectNode = wordTemplate.children[1];
+	
+	// Add each custom color to selectNode as an option node.
+	chrome.storage.sync.get(null,function(colors){
+		
+		for(var color in colors){
+			var option = document.createElement("option");
+			option.innerHTML = color;
+			option.value = colors[color];
+			option.id = color;
+			selectNode.appendChild(option);
+		} 
+	});
+}
+
+/*
+*-----------------------------------------------------------
+* These functions control content script and css injection
+*-----------------------------------------------------------
+*/
+
 // Gets the words and their respective colors and then sends that information to the current tab.
-// TODO: Deal with colors that are not blue red green or purple in the receiving end of the message.
 function highlightSendData(){	
 	// JSON string containing word and color values
 	var wordsAndColors = getWordsAndColors();
@@ -63,9 +230,7 @@ function removeHighlighting(){
 	chrome.tabs.insertCSS({file:'highlight.css'});	
 }
 
-// TODO: Deal with all colors that are no blue red green or purple. Add and if statement
-// and collect information as rgb string from the html option tag.  Place rgb information 
-// into the html page. 
+// This function gets the words and colors currently selected by the user.
 function getWordsAndColors(){
 	var wordsAndColors = {};
 	var baseColors = ["blue", "red", "green", "purple"];
@@ -86,62 +251,11 @@ function getWordsAndColors(){
 	return wordsAndColors;
 }
 
-// Toggle the display of the new color section
-function toggleRGBInputDisplay(){
-	var rgbInputDiv = document.getElementById('rgb-input');
-	var currentDisplay = rgbInputDiv.style.display;
-	
-	if(currentDisplay == "none"){
-		rgbInputDiv.style.display = "block";
-	}else{
-		rgbInputDiv.style.display = "none";
-	}
-
-}
-
-function updateColorPreview(){
-	var blueValue = document.getElementById("new-blue-value").value;
-	var redValue = document.getElementById("new-red-value").value;
-	var greenValue = document.getElementById("new-green-value").value;
-
-	if(blueValue && redValue && greenValue){
-		var colorPreview = document.getElementById("color-preview");
-		colorPreview.style.color = "rgb("+redValue+","+greenValue+","+blueValue+")";
-	}	
-}
-
 /*
-* This function gets the name of the new color and the rgb values and then updates all the 
-* drop down menus with the name of the new color.
+*-------------------------------------------------------------------
+* Event Listeners
+*-------------------------------------------------------------------
 */
-function createNewColor(){
-	var newColorName = document.getElementById('new-color-name').value;
-	var redValue = document.getElementById('new-red-value').value;
-	var greenValue = document.getElementById('new-green-value').value;
-	var blueValue = document.getElementById('new-blue-value').value;
-	
-	// Reset all of the values and the color of the color preview
-	document.getElementById('new-color-name').value = "";
-	document.getElementById('new-red-value').value = "";
-	document.getElementById('new-blue-value').value = "";
-	document.getElementById('new-green-value').value = "";
-	document.getElementById('color-preview').style.color = "black";
-	
-	var rgbString = "rgba("+redValue+","+greenValue+","+blueValue+",0.5)";
-
-	var allSelectTags = document.getElementsByTagName("select");
-	
-	// For each select tag append the name of the new color as an option
-	for(var i=0;i<allSelectTags.length;i++){
-		var newOption = document.createElement("option");
-		var optionText = document.createTextNode(newColorName);
-		newOption.setAttribute("value",rgbString);
-		newOption.appendChild(optionText);
-		
-		allSelectTags[i].appendChild(newOption);
-	}	
-	toggleRGBInputDisplay();
-}
 
 // Event listener for adding a new word and color choice to the extension
 document.getElementById('another-word').addEventListener('click',newWord);
@@ -166,3 +280,19 @@ document.getElementById('new-red-value').addEventListener("input", updateColorPr
 document.getElementById('new-green-value').addEventListener("input", updateColorPreview);
 document.getElementById('new-blue-value').addEventListener("input", updateColorPreview);
 
+// Event listeners for managing the custom colors
+document.getElementById('manage-colors').addEventListener('click',toggleManageColorDisplay);
+
+// Event listener for the remove button for the words
+document.getElementById('manage-colors-display').addEventListener('click',function(e){
+	var targetNode = e.target;
+	if(targetNode.matches('.remove-word')){
+		var color = targetNode.value
+		// Pass the node to be removed to the removeWordHandler. 
+		removeNode(targetNode);
+		//TODO: REMOVE THE COLOR FROM THE SELECT NODE.
+		removeColorFromAllSelectNodes(color);
+		removeColorFromStorage(targetNode.value);
+	}
+});
+window.addEventListener('load', addCustomColorsToSelect);
